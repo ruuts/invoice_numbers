dd5adeeb2ea1414956688898ccce73b04f9f6b64
Rob <rob@ikdoe.nl>
2011-09-06 16:31:18 +0200

    create compatibility with rails < 3.0 by not using the new migrations.

--- lib/generators/templates/migration.rb
+++ lib/generators/templates/migration.rb
@@ -1,10 +1,14 @@
 class CreateInvoiceNumberSequences < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :invoice_number_sequences do |t|
       t.string  :name
       t.integer :next_number, :default => 1
     end
 
     add_index :invoice_number_sequences, [:name], :unique => true
+  end
+
+  def self.down
+    drop_table :invoice_number_sequences
   end
 end
